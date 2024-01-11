06c1c585e6840598b709a8edf8b0bdd31095b9be
Florencia Marchiondelli <fflor777@hotmail.com>
2024-01-11 13:43:01 -0300

    botones y filtrado de categorías en Guides

--- client/src/components/NavBar/items/Guides.jsx
+++ client/src/components/NavBar/items/Guides.jsx
@@ -1,10 +1,85 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux"
+import { getAllGuides } from "../../../redux/actions/guidesActions";
 
 const Guides = () => {
+    const dispatch = useDispatch();
+    const guides = useSelector(state => state.guide.allGuides)
+    console.log("Guias de la db: ", guides)
+
+    const [showNutritionGuides, setShowNutritionGuides] = useState(false);
+    const handleOnPressNutrition = () => {
+        setShowNutritionGuides(!showNutritionGuides);
+        console.log("Botón nutrición")
+    }
+
+    const [showTrainingGuides, setShowTrainingGuides] = useState(false)
+    const handleOnPressTraining = () => {
+        setShowTrainingGuides(!showTrainingGuides);
+        console.log("Botón Entrenamiento")
+    }
+
+    const [showTrainingAndNutrition, setTrainingAndNutrition] = useState(false)
+    const handleOnPressTrainingAndNutrition = () => {
+        setTrainingAndNutrition(!showTrainingAndNutrition);
+        console.log("Botón Entrenamiento y nutrición")
+    }
+
+    const nutritionGuide = guides.filter(guide => guide.category === "Nutrición")
+    const trainingGuide = guides.filter(guide => guide.category === "Entrenamiento")
+    const trainingAndNutritionGuide = guides.filter(guide => guide.category === "trainingNutrition")
+
+    useEffect(() => {
+        dispatch(getAllGuides())
+    }, []);
+
     return (
         <div>
             <div>
-                <text>Estas son las guías</text>
+                <h2>¿Cuál es para mi?</h2>
+                <div>
+                    <button onClick={handleOnPressNutrition}>
+                        <h3>Guías de Nutrición</h3>
+                    </button>
+                </div>
+                {showNutritionGuides && (
+                    <div>
+                        <ul>
+                            {nutritionGuide?.map(guide => (
+                                <li key={guide.id}>{guide.title}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+                <div>
+                    <button onClick={handleOnPressTraining}>
+                        <h3>Guías de Entrenamiento</h3>
+                    </button>
+                </div>
+                {showTrainingGuides && (
+                    <div>
+                        <ul>
+                            {trainingGuide?.map(guide => (
+                                <li key={guide.id}>{guide.title}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+                <div>
+                    <button onClick={handleOnPressTrainingAndNutrition}>
+                        <h3>Guías de Entrenamiento y Nutrición</h3>
+                    </button>
+                </div>
+                {showTrainingAndNutrition && (
+                    <div>
+                        <ul>
+                            {trainingAndNutritionGuide?.map(guide => (
+                                <li key={guide.id}>{guide.title}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
             </div>
         </div>
     )
--- client/src/redux/actions/guidesActions.js
+++ client/src/redux/actions/guidesActions.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 
 //Todas las guías
 export const getAllGuides = () => async (dispatch) => {
-    console.log('SE EJECUTÓ LA ACTION')
     let response = await axios.get('http://localhost:3001/guides')
     let allGuides = response.data
     dispatch(guides(allGuides))
