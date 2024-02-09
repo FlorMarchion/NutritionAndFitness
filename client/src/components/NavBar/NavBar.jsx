0cfaf8069fc019a0848afd8b23726769799832b5
Florencia Marchiondelli <fflor777@hotmail.com>
2024-02-09 18:02:12 -0300

    Agregando algunos estilos a navBar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import "../../styles/navBar.css"
 
 const NavBar = () => {
     const location = useLocation();
@@ -8,36 +9,34 @@ const NavBar = () => {
         <>
             {
                 location.pathname.toString() !== "/" ? <nav>
-                    <ul>
-                        <li>
-                            <Link to="/plans">Planes</Link>
-                        </li>
-                        <li>
-                            <Link to="/guides">Guías</Link>
-                        </li>
-                        <li>
-                            <Link to="/challenges">Retos</Link>
-                        </li>
-                        <li>
-                            <Link to="/home">Giuliana Marchiondelli</Link>
-                        </li>
-                        <li>
-                            <Link to="/me">¿Quién soy?</Link>
-                        </li>
-                        <li>
-                            <Link to="/vlog">Vlog</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact">Contáctame</Link>
-                        </li>
-                    </ul>
+                    <div className="navegation-container">
+                        <ul className="navegation-items-list">
+                            <li>
+                                <Link className="nav-link" to="/plans">Planes</Link>
+                            </li>
+                            <li>
+                                <Link className="nav-link" to="/guides">Guías</Link>
+                            </li>
+                            <li>
+                                <Link className="nav-link" to="/challenges">Retos</Link>
+                            </li>
+                            <li>
+                                <Link className="nav-link" to="/home">Giuliana Marchiondelli</Link>
+                            </li>
+                            <li>
+                                <Link className="nav-link" to="/me">¿Quién soy?</Link>
+                            </li>
+                            <li>
+                                <Link className="nav-link" to="/vlog">Vlog</Link>
+                            </li>
+                            <li>
+                                <Link className="nav-link" to="/contact">Contáctame</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </nav> : null
             }
-
-
         </>
-
     )
-
 }
 export default NavBar;
